b85ec0743875f91a57c7713bf92c4113594e728f
Pablo Cianes <cianespablo@gmail.com>
2022-02-14 10:05:58 +0100

    🚧 react-basic add final proyect to do exercises

--- react/basic/src/App.js
+++ react/basic/src/App.js
@@ -2,17 +2,19 @@ import './App.css';
 //import ComponentA from './components/componentA';
 //import Clock from './components/clock'
 //import Contactlist from './components/container/contactList';
-import MouseEvents from './components/mouseEvents';
+//import MouseEvents from './components/mouseEvents';
+import TaskListComponent from './components/container/task_list';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <ComponentA></ComponentA> */}
-        {/* <Clock></Clock> */}
-        {/* <ContactList></ContactList> */}
-        <MouseEvents></MouseEvents>
-      </header>
+      {/* <header className="App-header"> */}        {/* <ComponentA></ComponentA> */}
+      {/* <Clock></Clock> */}
+      {/* <ContactList></ContactList> */}
+      {/*<MouseEvents></MouseEvents>
+      {/* </header> */}
+      {/* PROYECTO FINAL */}
+      {<TaskListComponent></TaskListComponent>}
     </div>
   );
 }
