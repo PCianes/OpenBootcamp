73cb191376309003d51b0adcba71ca95417fc4b9
Pablo Cianes <cianespablo@gmail.com>
2022-02-14 10:48:59 +0100

    📋♻️ react-basic exercise session 13-14-15 formik

--- react/basic/src/components/pure/forms/taskForm.jsx
+++ react/basic/src/components/pure/forms/taskForm.jsx
@@ -1,47 +1,98 @@
-import React, {useRef} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { LEVELS } from '../../../models/levels.enum';
 import { Task } from '../../../models/task.class';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
-const Taskform = ({add, length}) => {
+const initialValues = {
+    title: '',
+    description: '',
+    level: LEVELS.NORMAL
+}
 
-    const nameRef = useRef('');
-    const descriptionRef = useRef('');
-    const levelRef = useRef(LEVELS.NORMAL);
+const validationSchema = Yup.object().shape(
+    {
+        title: Yup.string()
+            .max(12, 'Title too long')
+            .required('Title is required'),
+        description: Yup.string()
+            .required('Description is required'),
+        level: Yup.string()
+            .oneOf([LEVELS.BLOCKING, LEVELS.NORMAL, LEVELS.URGENT], 'You must select a Level')
+            .required('Level is required')
+    }
+);
 
-    function addTask(e){
-        e.preventDefault();
+const Taskform = ({ add, length }) => {
+
+    function addTask(values, reset) {
         const newTask = new Task(
-            nameRef.current.value,
-            descriptionRef.current.value,
+            values.title,
+            values.description,
             false,
-            levelRef.current.value
+            values.level
         );
-        add(newTask);
+        add(newTask)
+        reset()
     }
 
     return (
-        <form onSubmit={addTask} className='d-flex justify-content-center align-items-center mb-4'>
-            <div className='form-outline flex-fill'>
-                <input ref={nameRef} id='inputName' type='text' className='form-control form-control-lg' required autoFocus placeholder='Task Name'/>
-                <input ref={descriptionRef} id='inputDescription' type='text' className='form-control form-control-lg' required placeholder='Task description'/>
-                <select className='form-control form-control-lg' ref={levelRef} defaultValue={LEVELS.NORMAL} id='selectLevel'>
-                    <option value={LEVELS.NORMAL}>
-                        Normal
-                    </option>
-                    <option value={LEVELS.URGENT}>
-                        Urgent
-                    </option>
-                    <option value={LEVELS.BLOCKING}>
-                        Blocking
-                    </option>
-                </select>
-                <button type='submit' className='btn btn-success btn-lg ms-2'>
-                    {length > 0 ? 'Add New Task' : 'Create your First Task'}
-                </button>
-            </div>
-        </form>
-    );
+        <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={(values, { resetForm }) => { addTask(values, resetForm) }}
+        >
+            {
+                ({ touched, errors }) => (
+                    <Form className='d-flex justify-content-center align-items-center mb-4'>
+                        <div className='form-outline flex-fill'>
+                            <label htmlFor="title">Title</label>
+                            <Field id="title" type="text" name="title" placeholder="Task title" className='form-control form-control-lg' />
+                            {
+                                errors.title && touched.title &&
+                                (
+                                    <ErrorMessage name="title" component='div'></ErrorMessage>
+                                )
+                            }
+
+                            <label htmlFor="description">Description</label>
+                            <Field id="description" type="text" name="description" placeholder="Task description" className='form-control form-control-lg'
+                            />
+                            {
+                                errors.description && touched.description &&
+                                (
+                                    <ErrorMessage name="description" component='div'></ErrorMessage>
+                                )
+                            }
+                            <label htmlFor="level">Level</label>
+                            <Field id="level" name="level" component="select" className='form-control form-control-lg'>
+                                <option value={LEVELS.NORMAL}>
+                                    Normal
+                                </option>
+                                <option value={LEVELS.URGENT}>
+                                    Urgent
+                                </option>
+                                <option value={LEVELS.BLOCKING}>
+                                    Blocking
+                                </option>
+                            </Field>
+                            {
+                                errors.level && touched.level &&
+                                (
+                                    <ErrorMessage name="level" component='div'></ErrorMessage>
+                                )
+                            }
+
+                            <button type="submit" className='btn btn-success btn-lg mt-3'>
+                                {length > 0 ? 'Add New Task' : 'Create your First Task'}
+                            </button>
+                        </div>
+                    </Form>
+                )
+            }
+        </Formik>
+    )
 }
 
 Taskform.protoTypes = {
