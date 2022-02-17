911a45200f4980f7530e249f8b535d518d9fff6e
Pablo Cianes <cianespablo@gmail.com>
2022-02-17 14:11:12 +0100

    🐛 fix bug: react basic final project

--- react/doc/basic-final/src/components/settings/Settings.jsx
+++ react/doc/basic-final/src/components/settings/Settings.jsx
@@ -2,32 +2,39 @@ import React from 'react';
 import useLocalStorage from '../../hooks/useLocalStorage';
 
 const defaultConfig = {
-    theme: 'dark',
-    lang: 'es',
+  theme: 'dark',
+  lang: 'es',
 };
 
 export default function Settings() {
-    const [config, setConfig] = useLocalStorage('config', defaultConfig);
+  const [config, setConfig] = useLocalStorage('config', defaultConfig);
 
-    const handleClick = (event) => {
-        event.preventDefault();
-        setConfig((oldConfig) => (
-            {
-            ...oldConfig,
-            theme: 'light',
-            }));
-    };
+  const handleClick = (event) => {
+    event.preventDefault();
+    setConfig((oldConfig) => (
+      {
+        ...oldConfig,
+        theme: 'light',
+      }));
+  };
 
-    return (
-      <div>
-        <h1>APP SETTINGS</h1>
-        <p>
-          Actual Config:
-          { config }
-        </p>
-        <button type="button" onClick={handleClick}>
-          Save New Settings
-        </button>
-      </div>
-    );
+  return (
+    <div>
+      <h1>APP SETTINGS</h1>
+      <h3>Actual Config</h3>
+      <p>
+        lang:
+        {' '}
+        {config.lang}
+      </p>
+      <p>
+        theme:
+        {' '}
+        {config.theme}
+      </p>
+      <button type="button" onClick={handleClick}>
+        Save New Settings
+      </button>
+    </div>
+  );
 }
