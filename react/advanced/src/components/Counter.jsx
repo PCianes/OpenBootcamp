16e949097fbf2a8ec5f8f736589ccf12dc1c30c2
Pablo Cianes <cianespablo@gmail.com>
2022-02-20 10:32:11 +0100

    🚧 react advanced exercise session 2-3

---
+++ react/advanced/src/components/Counter.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useCounter } from '../hooks/useCounter'
+
+export function Counter() {
+  const { counter, setCounter } = useCounter(10, 5, 100)
+
+  return (
+    <div>
+      <h1>¡Hello Counter!</h1>
+      <p style={{ color: 'tomato', fontWeight: 'bold' }}>{counter}</p>
+      <button onClick={setCounter.increment}>Increment</button>
+      <button onClick={setCounter.decrement}>Decrement</button>
+      <button onClick={setCounter.reset}>Reset</button>
+      <input type="number" placeholder="1" min="0" onChange={(step) => setCounter.newStep(step.target.value)} />
+    </div>
+  )
+}
