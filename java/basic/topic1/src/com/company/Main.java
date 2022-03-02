c8f8dfaf3ab915f1ed38e7b5049c0263a84a1bf5
Pablo Cianes <cianespablo@gmail.com>
2022-03-02 11:57:59 +0100

    📋 java basic exercise topic1

---
+++ java/basic/topic1/src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        byte variable1 = 1;
+        short variable2 = 12;
+        int variable3 = 555;
+        long variable4 = 123434;
+
+        float variable5 = 3.45f;
+        double variable6 = 45.7788d;
+
+        char variable7 = 'p';
+        String variable8 = "Hello World";
+
+        boolean variable9 = true;
+        boolean variable10 = false;
+
+        System.out.println(variable1);
+        System.out.println(variable2);
+        System.out.println(variable3);
+        System.out.println(variable4);
+        System.out.println(variable5);
+        System.out.println(variable6);
+        System.out.println(variable7);
+        System.out.println(variable8);
+        System.out.println(variable9);
+        System.out.println(variable10);
+    }
+}
